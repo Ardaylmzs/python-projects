d94598ac49e134c2414b214cde045d57e470c12e
Arda Yılmaz <Arda_8773@outlook.com>
2025-09-06 20:28:56 +0300

    this file is updated , this program send automatically message that you saved date in csv file if you connect it a cloud system

---
+++ birthday-message/main.py
@@ -0,0 +1,22 @@
+import smtplib
+import random
+from datetime import datetime
+import pandas
+MY_EMAIL = "your e-mail address"
+MY_PASSWORD ="your app password"
+
+today = datetime.now()
+today_tuple  = (today.month ,today.day)
+data = pandas.read_csv("birthdays.csv")
+data_dict ={(data_row["month"], data_row["day"]) : data_row for (index, data_row) in data.iterrows()}
+if today_tuple in data_dict:
+    birthday_person = data_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        letter = letter_file.read()
+        letter = letter.replace("[NAME]" , birthday_person["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"BIRTHDAY MESSAGE\n\n{letter}")
+
