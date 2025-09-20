8326210967593cc3b7fe47a773c1e462d09e7b01
Abstract Programmer <AbstractProgrammer@example.com>
2025-09-20 04:23:01 +0100

    📦 Add initial project structure 📝 Add sample birthdays.csv and letter templates
    🐛 Fix Windows path issue when reading CSV

--- birthday-message/main.py
+++ birthday-message/main.py
@@ -1,13 +1,20 @@
 import smtplib
 import random
 from datetime import datetime
-import pandas
-MY_EMAIL = "your e-mail address"
-MY_PASSWORD ="your app password"
+import pandas as pd
+
+
+MY_EMAIL = "your-email@gmail.com"
+MY_PASSWORD ="your-app-password"
 
 today = datetime.now()
 today_tuple  = (today.month ,today.day)
-data = pandas.read_csv("birthdays.csv")
+try:
+    data = pd.read_csv("Ardaylmzs-python-projects/birthday-message/birthdays.csv")
+except FileNotFoundError:
+    print("Error: 'birthdays.csv' file not found")
+    exit()
+
 data_dict ={(data_row["month"], data_row["day"]) : data_row for (index, data_row) in data.iterrows()}
 if today_tuple in data_dict:
     birthday_person = data_dict[today_tuple]
@@ -18,5 +25,5 @@ if today_tuple in data_dict:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"BIRTHDAY MESSAGE\n\n{letter}")
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg = f"Subject: Happy Birthday!\n\n{letter}")
 
